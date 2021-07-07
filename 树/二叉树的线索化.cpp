d7e45a9b7b36d803960b890eae1c3a474213e106
JimouChen <jmc1625645320@gmail.com>
2021-07-07 17:36:56 +0800

    update

---
+++ 树/二叉树的线索化.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct CueingNode {
+    int data;
+    struct CueingNode *lchild, *rchild;
+    int ltag, rtag;
+} CueingNode, *CueingTree;
+
+//定义一个全局的前驱指针
+CueingNode *pre = NULL;
+
+void initThread(CueingTree t) {
+    pre = NULL;
+    if (t) {  //非空二叉树才可以线索化
+        InThread(t);
+        if (pre->rchild == NULL)  //处理最后一个节点
+            pre->rtag = 1;
+    }
+}
+
+void visit(CueingTree t) {
+    if (t->lchild) {
+        t->lchild = pre;
+        t->ltag = 1;
+    }
+    //右孩子不存在,且此时t已经走到了下一个节点
+    //也就是t现在是pre的后继节点
+    if (pre && !pre->rchild) {
+        pre->rchild = t;  //右孩子指针指向后继节点
+        pre->rtag = 1;
+    }
+    pre = t;  //这句结束后，t就指向t的下一个访问节点
+}
+//中序线索化遍历
+void InThread(CueingTree t) {
+    if (t) {
+        InThread(t->lchild);
+        visit(t);
+        InThread(t->rchild);
+    }
+}
+int main() { return 0; }
