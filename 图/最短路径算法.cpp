9e0dec59959e2194a4598e5a8d887c0c0cb2aa58
JimouChen <jmc1625645320@gmail.com>
2021-07-11 23:14:48 +0800

    update

--- 图/最短路径算法.cpp
+++ 图/最短路径算法.cpp
@@ -55,8 +55,60 @@ void dijkstra(MGraph g, int v) {
     //得到path和dist后再显示处理
 }
 
+//佛洛依德算法，动态规划，三层遍历
+//需要实现准备2个二维数组a,path
+void floyd(MGraph g) {
+    MGraph a = g;
+    int path[g.vexNum][g.vexNum];
+    for (int i = 0; i < g.vexNum; i++)
+        for (int j = 0; j < g.vexNum; j++)
+            path[i][j] = -1;
+
+    for (int k = 0; k < g.vexNum; k++)//k是中转点
+        for (int i = 0; i < g.vexNum; i++)
+            for (int j = 0; j < g.vexNum; j++) {
+                if (a.edge[i][j] > a.edge[i][k] + a.edge[k][j]) {
+                    a.edge[i][j] = a.edge[i][k] + a.edge[k][j];
+                    path[i][j] = k;//记录中转点（上一个点）
+                }
+            }
+
+    //得到记录最短路径的a和前驱path，可做输出显示
+
+    for (int i = 0; i < g.vexNum; i++) {
+        for (int j = 0; j < g.vexNum; j++)
+            cout << a.edge[i][j] << " ";
+        cout << endl;
+    }
+
+    for (int i = 0; i < g.vexNum; i++) {
+        for (int j = 0; j < g.vexNum; j++)
+            cout << path[i][j] << " ";
+        cout << endl;
+    }
+
+
+}
+
 int main() {
-    cout << "hello";
+
+    MGraph graph;
+    graph.vexNum = 5;
+    graph.edgeNum = 7;
+    int temp[5][5] = {
+            {0,   INF, 1,   INF, 10},
+            {INF, 0,   INF, 1,   5},
+            {INF, 1,   0,   INF, 7},
+            {INF, INF, INF, 0,   1},
+            {INF, INF, INF, INF, 0}
+    };
+
+    for (int i = 0; i < graph.vexNum; i++)
+        for (int j = 0; j < graph.vexNum; j++)
+            graph.edge[i][j] = temp[i][j];
+
+    floyd(graph);
+
 
     return 0;
 }
