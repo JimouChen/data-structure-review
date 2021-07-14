d1eed99147691d16b5a915bc53e6dfd4c52781aa
JimouChen <jmc1625645320@gmail.com>
2021-07-14 16:15:38 +0800

    update

--- 排序/排序算法.cpp
+++ 排序/排序算法.cpp
@@ -1,11 +1,23 @@
+#pragma once
+
 #include "排序算法.h"
 
 int main() {
     vector<int> test = {0, 2, 1, 6, 4, 3, 76, 50, 33};
+    vector<int> test2 = {0, 2, 1, 6, 4, 3, 76, 50, 33};
+    int nums[9] = {10, 2, 1, 6, 4, 3, 76, 50, 33};
 //    insertSort(test);
 //    biInsertSort(test);
-    shellSort(test);
+//    shellSort(test);
+    bubbleSort(test);
     show(test);
+//    testLinkBubble();
+
+    quickSort(nums, 0, 8);
+    for (int i = 0; i < 9; ++i)
+        cout << nums[i] << " ";
+    cout << endl;
+
 
     return 0;
 }
--- 排序/排序算法.h
+++ 排序/排序算法.h
@@ -61,3 +61,92 @@ void shellSort(vector<int> &nums) {
         }
     }
 }
+
+//冒泡排序
+void bubbleSort(vector<int> &nums) {
+    for (int i = 0; i < nums.size() - 1; i++) {
+        bool isExchange = false;
+        for (int j = i + 1; j < nums.size(); j++)
+            if (nums[i] > nums[j]) {
+                isExchange = true;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+    }
+
+}
+
+//链表的冒泡排序
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node, *linkNode;
+
+void linkBubble(linkNode l) {
+    Node *p = l->next;
+    while (p && p->next) {
+        Node *k = p->next;
+        while (k) {
+            if (k->data < p->data) {
+                int temp = p->data;
+                p->data = k->data;
+                k->data = temp;
+            }
+            k = k->next;
+        }
+        p = p->next;
+    }
+}
+
+void testLinkBubble() {
+    linkNode link = (Node *) malloc(sizeof(Node));
+    Node *node1 = (Node *) malloc(sizeof(Node));
+    Node *node2 = (Node *) malloc(sizeof(Node));
+    Node *node3 = (Node *) malloc(sizeof(Node));
+    Node *node4 = (Node *) malloc(sizeof(Node));
+
+    node1->data = 5;
+    node2->data = 2;
+    node3->data = 1;
+    node4->data = 4;
+
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = NULL;
+    link->next = node1;
+
+    linkBubble(link);
+    Node *p = link->next;
+    while (p) {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+//快速排序
+//找到正确的位置
+int findPosition(int nums[], int low, int high) {
+    //设置排序区间的第一个数是基准数
+    int firstNum = nums[low];
+    while (low < high) {
+        while (low < high && nums[high] > firstNum) --high;//跳出循环时说明找到一个比基准数小的数
+        nums[low] = nums[high];
+        while (low < high && nums[low] < firstNum) ++low;
+        nums[high] = nums[low];
+    }
+    //跳出循环时候，此时low = high就是基准数正确的位置
+    nums[low] = firstNum;
+    return low;
+}
+
+void quickSort(int nums[], int low, int high) {
+    if (low < high) {
+        int rightPosition = findPosition(nums, low, high);
+        //分治，对正确位置两边也使用快排
+        quickSort(nums, low, rightPosition - 1);
+        quickSort(nums, rightPosition + 1, high);
+    }
+}
